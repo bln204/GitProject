b79e0bb07786c4e34a274cee8fc6c86d4bedd7e3
bln204 <lenambui3@gmail.com>
2024-04-14 14:43:44 +0700

    file js của variable_js.html

---
+++ my.js
@@ -0,0 +1,25 @@
+/*let i = 10;
+let f = 20.5;
+let b = true;
+let s = 'Hà Nội';
+document.write('i =' + i);
+document.write('<br>');
+document.write('f = ' +f);
+document.write('<br>');
+document.write('b = ' +b);
+document.write('<br>');
+document.write('s = ' +s);
+document.write('<br>');
+document.write('<br>');
+document.write('<br>');
+document.write('<br>');
+
+let width = 20;
+let height = 10;
+let area = width * height;
+document.write('area = ' + area);*/
+
+let num_a = +prompt('a = ');
+let num_b = +prompt('b = ');
+let multiple = num_a%=num_b;
+alert  ('c = ' + multiple);
